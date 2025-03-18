0b36d9c3036bc3f34be9932492f531288c60b984
Lucas <lucasamarosouza1@gmail.com>
2025-03-18 14:54:05 -0300

    criando component

--- primeiros-passos-springboot/src/main/java/dio/springboot/PrimeirosPassosApplication.java
+++ primeiros-passos-springboot/src/main/java/dio/springboot/PrimeirosPassosApplication.java
@@ -8,10 +8,6 @@ public class PrimeirosPassosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrimeirosPassosApplication.class, args);
-
-		Calculadora calculadora = new Calculadora();
-
-		System.out.println("o resultado é " + calculadora.somar(2, 3));
 	}
 
 }
